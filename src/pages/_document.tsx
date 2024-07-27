44e38208ca8759741a267398c7b64bb1503b1e8e
pixelberry-jin <viva-jinyi@kakao.com>
2024-07-27 15:28:16 +0900

    fix: ogimage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,16 +7,31 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="강아지/고양이 별에 있는 반려동물이 보내주는 AI 편지 서비스" />
+        <meta
+          name="description"
+          content="강아지/고양이 별에 있는 반려동물이 보내주는 AI 편지 서비스"
+        />
         <meta property="og:title" content="젤리레터 🐾💌" />
-        <meta property="og:description" content="강아지/고양이 별에 있는 반려동물이 보내주는 AI 편지 서비스" />
-        <meta property="og:image" content="/images/og_image.png" />
+        <meta
+          property="og:description"
+          content="강아지/고양이 별에 있는 반려동물이 보내주는 AI 편지 서비스"
+        />
+        <meta
+          property="og:image"
+          content="https://jellyletter.netlify.app/og_image.png"
+        />
         <meta property="og:url" content="https://jellyletter.netlify.app/" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="젤리레터 🐾💌" />
-        <meta name="twitter:description" content="강아지/고양이 별에 있는 반려동물이 보내주는 AI 편지 서비스" />
-        <meta name="twitter:image" content="/images/og-image.png" />
+        <meta
+          name="twitter:description"
+          content="강아지/고양이 별에 있는 반려동물이 보내주는 AI 편지 서비스"
+        />
+        <meta
+          name="twitter:image"
+          content="https://jellyletter.netlify.app/og_image.png"
+        />
       </Head>
       <body>
         <Main />
