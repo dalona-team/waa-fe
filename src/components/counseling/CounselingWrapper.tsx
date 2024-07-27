50a44de952eba8d990d5685905e4ae4e998dd02f
pixelberry-jin <viva-jinyi@kakao.com>
2024-07-27 19:25:30 +0900

    fix: wrapper optiz

--- src/components/counseling/CounselingWrapper.tsx
+++ src/components/counseling/CounselingWrapper.tsx
@@ -4,15 +4,47 @@ import Image from 'next/image';
 const CounselingWrapper = ({ content, className }: { content: React.ReactNode, className?: string }) => {
   return (
     <div className={`relative w-full ${className} mb-8`}>
-      <Image priority className='absolute left-0 top-0' src="/images/letter_edges_yellow/letter_top_l.svg" alt="Letter edge" width={60} height={60} />
-      <Image priority className='absolute right-0 top-0' src="/images/letter_edges_yellow/letter_top_r.svg" alt="Letter edge" width={60} height={60} />
-      <Image priority className='absolute left-0 bottom-[-10px]' src="/images/letter_edges_yellow/letter_bottom_l.svg" alt="Letter edge" width={159} height={90} />
-      <Image priority className='absolute right-0 bottom-0' src="/images/letter_edges_yellow/letter_bottom_r.svg" alt="Letter edge" width={162} height={80} />
-      <div className='flex justify-center items-center h-[60px] bg-yellow ml-[60px] mr-[50px] pt-5'>
-        <span className='text-gray-950 font-bold'>COUNSELING</span>
+      <div className="flex justify-center items-center h-[60px]">
+        <Image
+          className="pt-2.5"
+          priority
+          src="/images/letter_edges_yellow/letter_top_l.svg"
+          alt="Letter edge"
+          width={60}
+          height={60}
+        />
+        <div className="bg-yellow flex-1 h-full flex justify-center items-center">
+          <span className="text-gray-950 font-bold">COUNSELING</span>
+        </div>
+        <Image
+          priority
+          src="/images/letter_edges_yellow/letter_top_r.svg"
+          alt="Letter edge"
+          width={60}
+          height={60}
+        />
       </div>
-      <div className='text-gray-950 bg-yellow z-10 relative text-base pt-2'>{content}</div>
-      <div className='flex justify-center items-center h-[80px] bg-yellow ml-[159px] mr-[162px]'></div>
+      <div className="text-gray-950 bg-yellow z-10 relative text-base pt-2">
+        {content}
+      </div>
+      <div className="flex justify-center items-center h-[80px]">
+        <Image
+          className='mt-2.5'
+          priority
+          src="/images/letter_edges_yellow/letter_bottom_l.svg"
+          alt="Letter edge"
+          width={159}
+          height={90}
+        />
+        <div className="bg-yellow flex-1 h-full flex justify-center items-center"></div>
+        <Image
+          priority
+          src="/images/letter_edges_yellow/letter_bottom_r.svg"
+          alt="Letter edge"
+          width={162}
+          height={80}
+        />
+      </div>
     </div>
   );
 };
--- src/components/letter/LetterWrapper.tsx
+++ src/components/letter/LetterWrapper.tsx
@@ -4,15 +4,50 @@ import Image from 'next/image';
 const LetterWrapper = ({ content, className }: { content: React.ReactNode, className?: string }) => {
   return (
     <div className={`relative w-full ${className}`}>
-      <Image priority className='absolute left-0 top-0' src="/images/letter_edges_green/letter_top_l.svg" alt="Letter edge" width={60} height={60} />
-      <Image priority className='absolute right-0 top-0' src="/images/letter_edges_green/letter_top_r.svg" alt="Letter edge" width={50} height={60} />
-      <Image priority className='absolute left-0 bottom-0' src="/images/letter_edges_green/letter_bottom_l.svg" alt="Letter edge" width={162} height={80} />
-      <Image priority className='absolute right-0  bottom-[-10px]' src="/images/letter_edges_green/letter_bottom_r.svg" alt="Letter edge" width={159} height={90} />
-      <div className='flex justify-center items-center h-[60px] bg-primary ml-[60px] mr-[50px]'>
-        <Image priority src="/images/logo_white.svg" alt="BigLogoImage" width={24} height={24} />
+      <div className="flex justify-center items-center h-[60px]">
+        <Image
+          priority
+          src="/images/letter_edges_green/letter_top_l.svg"
+          alt="Letter edge"
+          width={60}
+          height={60}
+        />
+        <div className="bg-primary flex-1 h-full flex justify-center items-center">
+          <Image
+            priority
+            src="/images/logo_white.svg"
+            alt="BigLogoImage"
+            width={24}
+            height={24}
+          />
+        </div>
+        <Image
+          priority
+          src="/images/letter_edges_green/letter_top_r.svg"
+          alt="Letter edge"
+          width={50}
+          height={60}
+        />
       </div>
-      <div className='text-white bg-primary'>{content}</div>
-      <div className='flex justify-center items-center h-[80px] bg-primary ml-[162px] mr-[159px]'></div>
+      <div className="text-white bg-primary">{content}</div>
+      <div className="flex justify-center items-center h-[80px]">
+        <Image
+          priority
+          src="/images/letter_edges_green/letter_bottom_l.svg"
+          alt="Letter edge"
+          width={162}
+          height={80}
+        />
+        <div className="bg-primary flex-1 h-full flex justify-center items-center"></div>
+        <Image
+          className='mt-2.5'
+          priority
+          src="/images/letter_edges_green/letter_bottom_r.svg"
+          alt="Letter edge"
+          width={159}
+          height={90}
+        />
+      </div>
     </div>
   );
 };
