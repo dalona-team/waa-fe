bd033d007fd063d4da97a09b2e7a9ca9b302d115
pixelberry-jin <viva-jinyi@kakao.com>
2024-07-28 12:05:13 +0900

    fix: main styling

--- src/components/home/HomeNight.tsx
+++ src/components/home/HomeNight.tsx
@@ -32,37 +32,109 @@ const HomeNight = () => {
   const router = useRouter();
 
   return (
-    <div className='relative flex flex-col items-center justify-center h-full overflow-y-auto invisible-scroll py-6'>
-      <Image priority className='absolute top-[12%] left-[15%]' src="/images/night_animals/illust_01_dark.svg" alt="강아지들" width={60} height={80} />
-      <Image priority className='absolute top-[22%] right-[9%]' src="/images/night_animals/illust_02_dark.svg" alt="강아지들" width={74} height={70} />
-      <Image priority className='slideLeft absolute top-[40%] left-[40%]' src="/images/night_animals/illust_03_dark.svg" alt="강아지들" width={80} height={60} />
-      <Image priority className='mt-[130px]' src="/images/logo_big_white.svg" alt="BigLogoImage" width={90} height={74} />
-      <p className='text-white text-center text-xs pt-1'>떠나간 반려동물에게<br />편지를 받아보세요!</p>
-      <Swiper
-        freeMode={true}
-        spaceBetween={6}
-        slidesPerView='auto'
-        className='w-full !pt-[196px] !pl-[20px] min-h-[380px]'
-      >
-        {cardList.map((card, index) => (
-          <SwiperSlide key={index} style={{width: 'auto', userSelect: 'none'}}>
-            <div className='relative w-40 h-[184px] font-gangwon leading-tight pl-5 pr-5 pt-2.5 pb-4 bg-white rounded-xl flex flex-col justify-center text-lg font-normal text-left'>
-              {card.imageSrc ? <div className='absolute left-0 w-full flex justify-center' style={{bottom: '96%'}}><Image src={card.imageSrc} alt="강아지" width={card.imageWidth} height={card.imageHeight} /></div> : null}
-              <div className='text-black/40 pb-1 truncate font-gangwon'>{card.title}</div>
-              <div className='text-black/80 text-ellipsis overflow-hidden line-clamp-6 font-gangwon'>{card.message}</div>
-            </div>
-          </SwiperSlide>
-        ))}
-        <SwiperSlide style={{width: 'auto', userSelect: 'none'}}>
-          <Image priority className='my-10' src="/images/night_animals/illust_07_dark.svg" alt="강아지들" width={200} height={100} style={{marginLeft: '-6px'}} />
-        </SwiperSlide>
-      </Swiper>
-      <div className='mt-4 mb-8'>
-        <button role='button' className='h-[52px] px-7 py-3 bg-primary rounded-[20px] shadow flex justify-center items-center' onClick={() => router.push('/form/step1')}>
-          <span className='text-center text-white text-sm font-bold'>내새꾸에게 편지 신청</span>
-        </button>
+    <div className="relative flex flex-col items-center justify-center h-full overflow-y-auto invisible-scroll">
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <Image
+          priority
+          className="absolute top-[12%] left-[15%]"
+          src="/images/night_animals/illust_01_dark.svg"
+          alt="강아지들"
+          width={60}
+          height={80}
+        />
+        <Image
+          priority
+          className="absolute top-[22%] right-[9%]"
+          src="/images/night_animals/illust_02_dark.svg"
+          alt="강아지들"
+          width={74}
+          height={70}
+        />
+        <Image
+          priority
+          className="slideLeft absolute top-[40%] left-[40%]"
+          src="/images/night_animals/illust_03_dark.svg"
+          alt="강아지들"
+          width={80}
+          height={60}
+        />
+        <div className="flex flex-col items-center relative top-[15px]">
+          <Image
+            priority
+            src="/images/logo_big_white.svg"
+            alt="BigLogoImage"
+            width={90}
+            height={74}
+          />
+          <p className="text-white text-center text-xs pt-1">
+            떠나간 반려동물에게
+            <br />
+            편지를 받아보세요!
+          </p>
+        </div>
       </div>
-      <div className="opacity-50 text-center text-white text-xs">Copyright by ⓒ 젤리레터. All rights reserved.</div>
+      <div className="flex-1 w-full overflow-hidden flex flex-col items-center gap-4">
+        <Swiper
+          freeMode={true}
+          spaceBetween={6}
+          slidesPerView="auto"
+          className="w-full !pl-[20px] !pt-[45px] min-h-[230px]"
+        >
+          {cardList.map((card, index) => (
+            <SwiperSlide
+              key={index}
+              style={{ width: "auto", userSelect: "none" }}
+            >
+              <div className="relative w-40 h-[184px] font-gangwon leading-tight pl-5 pr-5 pt-2.5 pb-4 bg-white rounded-xl flex flex-col justify-center text-lg font-normal text-left">
+                {card.imageSrc ? (
+                  <div
+                    className="absolute left-0 w-full flex justify-center"
+                    style={{ bottom: "96%" }}
+                  >
+                    <Image
+                      src={card.imageSrc}
+                      alt="강아지"
+                      width={card.imageWidth}
+                      height={card.imageHeight}
+                    />
+                  </div>
+                ) : null}
+                <div className="text-black/40 pb-1 truncate font-gangwon">
+                  {card.title}
+                </div>
+                <div className="text-black/80 text-ellipsis overflow-hidden line-clamp-6 font-gangwon">
+                  {card.message}
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+          <SwiperSlide style={{ width: "auto", userSelect: "none" }}>
+            <Image
+              priority
+              className="my-10"
+              src="/images/night_animals/illust_07_dark.svg"
+              alt="강아지들"
+              width={200}
+              height={100}
+              style={{ marginLeft: "-6px" }}
+            />
+          </SwiperSlide>
+        </Swiper>
+        <div className="flex-1 flex flex-col justify-between">
+          <button
+            role="button"
+            className="h-[52px] px-7 py-3 bg-primary rounded-[20px] shadow flex justify-center items-center"
+            onClick={() => router.push("/form/step1")}
+          >
+            <span className="text-center text-white text-sm font-bold">
+              내새꾸에게 편지 신청
+            </span>
+          </button>
+          <div className="opacity-50 text-center text-white text-xs mb-4">
+            Copyright by ⓒ 젤리레터. All rights reserved.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
