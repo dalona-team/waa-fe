dfdc034b4ba413c24170b4fd840f0fe269532c20
pixelberry-jin <viva-jinyi@kakao.com>
2024-08-13 13:52:18 +0900

    feature: myletter design modified

--- src/pages/my-letter/index.tsx
+++ src/pages/my-letter/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import Wrapper from '@/components/wrapper/Wrapper';
 import { useRouter } from 'next/router';
@@ -6,11 +7,19 @@ import Head from 'next/head';
 import { useModal } from '@/hooks/useModal';
 import Drawer from '@/components/drawer/Drawer';
 import Cookies from 'js-cookie';
+import moment from 'moment';
 
 interface Letter {
   shareKey: string;
   content: string;
   typeCode: number;
+  createAt: string;
+  petAiImage: {
+    id: number;
+    imageUrl: string;
+    message: string;
+    species: 'DOG' | 'CAT';
+  };
 }
 
 export default function MyLetter() {
@@ -98,48 +107,72 @@ export default function MyLetter() {
         formElement={
           data.length > 0 ? (
             <>
-              <div className='flex flex-col gap-3 mt-3'>
+              <div className="flex flex-col gap-3 mt-3">
                 {data.map((letter, index) => (
                   <div
                     key={letter.shareKey}
-                    className='h-[138px] overflow-hidden rounded-lg flex'
-                    onClick={() => letter.typeCode === 0 ? router.push(`/letter/${letter.shareKey}`) : router.push(`/letter/${letter.shareKey}?isReply=true`)}
+                    className="h-[138px] overflow-hidden rounded-lg flex"
+                    onClick={() =>
+                      letter.typeCode === 0
+                        ? router.push(`/letter/${letter.shareKey}`)
+                        : router.push(`/letter/${letter.shareKey}?isReply=true`)
+                    }
                   >
-                    <div className={`w-[10px] ${letter.typeCode === 0 ? 'bg-primary' : 'bg-accent'}`}></div>
-                    <div className='flex flex-col gap-2 p-5 bg-white flex-1'>
-                      <span className='text-black/80 text-lg font-bold'>{data.length - index}st {letter.typeCode === 0 ? '받은' : '보낸'} 편지</span>
-                      <span className='text-black/40 text-sm text-ellipsis overflow-hidden line-clamp-3'>{letter.content.replace(/\n/g, ' ')}</span>
+                    <div
+                      className={`w-[10px] ${
+                        letter.typeCode === 0 ? 'bg-primary' : 'bg-accent'
+                      }`}
+                    ></div>
+                    <div className="flex flex-col gap-2 p-5 bg-white flex-1">
+                      <div className="flex justify-between align-center">
+                        <span className="text-black/80 text-lg font-bold">
+                          {data.length - index}st{' '}
+                          {letter.typeCode === 0 ? '받은' : '보낸'} 편지
+                        </span>
+                        <span className="text-black/40 text-sm mt-1">
+                          {moment(letter.createAt).format('YYYY.MM.DD')}
+                        </span>
+                      </div>
+                      <div className='flex justify-between gap-2.5'>
+                        <span className="text-black/40 text-sm text-ellipsis overflow-hidden line-clamp-3 flex-1">
+                          {letter.content.replace(/\n/g, ' ')}
+                        </span>
+                        <div className='w-[60px] h-[60px]'>
+                          <img src={letter.petAiImage.imageUrl} width={60} height={60} alt={letter.petAiImage.species} />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 ))}
               </div>
-              <div className='text-black/40 text-sm mt-6'>
-                내새꾸와의 편지는 10회만 이용할 수 있습니다.편지 이용 횟수를 제한하는 이유는 보호자님이 편지를 통해 미완의 감정을 해결하고, 상실감으로부터 벗어날 수 있도록 도움을 드리고자 하는 이유입니다.
+              <div className="text-black/40 text-sm mt-6">
+                내새꾸와의 편지는 10회만 이용할 수 있습니다.편지 이용 횟수를
+                제한하는 이유는 보호자님이 편지를 통해 미완의 감정을 해결하고,
+                상실감으로부터 벗어날 수 있도록 도움을 드리고자 하는 이유입니다.
               </div>
             </>
-          ) :
-            (
-              <div className="h-full flex flex-col items-center justify-center">
-                <Image
-                  priority
-                  src="/images/logo_big.svg"
-                  alt="BigLogoImage"
-                  width={90}
-                  height={74}
-                />
-                <p className="text-black/80 text-center text-sm pt-1 mt-3 mb-6">
+          ) : (
+            <div className="h-full flex flex-col items-center justify-center">
+              <Image
+                priority
+                src="/images/logo_big.svg"
+                alt="BigLogoImage"
+                width={90}
+                height={74}
+              />
+              <p className="text-black/80 text-center text-sm pt-1 mt-3 mb-6">
                 받은 레터가 없습니다.
-                  <br />
+                <br />
                 내새꾸에게 레터를 신청해보세요!
-                </p>
-                <button
-                  className="w-[192px] h-12 text-white bg-primary rounded-[20px] flex justify-center items-center gap-1.5"
-                  onClick={() => router.push('/form/step1')}
-                >
-                  <span className="font-bold">내새꾸에게 편지신청</span>
-                </button>
-              </div>
-            )
+              </p>
+              <button
+                className="w-[192px] h-12 text-white bg-primary rounded-[20px] flex justify-center items-center gap-1.5"
+                onClick={() => router.push('/form/step1')}
+              >
+                <span className="font-bold">내새꾸에게 편지신청</span>
+              </button>
+            </div>
+          )
         }
       />
     </>
