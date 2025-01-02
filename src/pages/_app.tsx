72136a677086c0ac407c1a350e42a9a69fb6c31d
pixelberry-jin <viva-jinyi@kakao.com>
2025-01-02 10:53:59 +0900

    build: tanstack query 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,31 +5,36 @@ import '@/styles/global.css';
 import '@/styles/animation.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { FormProvider } from '@/hooks/useForm';
 import { ThemeModeProvider } from '@/hooks/useThemeMode';
 import { ThemeProvider } from '@mui/material/styles';
 import { ToastMessageProvider } from '@/hooks/useToastMessage';
 import ToastMessage from '@/components/toastMessage/ToastMessage';
 import { ModalProvider } from '@/hooks/useModal';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   const { layoutClassName, ...rest} = pageProps;
 
   return (
-    <ThemeProvider theme={theme}>
-      <ThemeModeProvider>
-        <FormProvider>
-          <ToastMessageProvider>
-            <ModalProvider>
-              <Layout className={layoutClassName}>
-                <Component {...rest} />
-                <ToastMessage />
-              </Layout>
-            </ModalProvider>
-          </ToastMessageProvider>
-        </FormProvider>
-      </ThemeModeProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <ThemeModeProvider>
+          <FormProvider>
+            <ToastMessageProvider>
+              <ModalProvider>
+                <Layout className={layoutClassName}>
+                  <Component {...rest} />
+                  <ToastMessage />
+                </Layout>
+              </ModalProvider>
+            </ToastMessageProvider>
+          </FormProvider>
+        </ThemeModeProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
